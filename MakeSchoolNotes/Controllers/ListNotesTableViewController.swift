6c436ce7e1d72b6ea9ec7de5728ecbce6ff1b97d
AbbyEF109 <abbyef109@gmail.com>
2017-08-24 15:00:35 -0400

    Finished section 7 - Introduction to Data Models

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,25 +10,31 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
     
+    var notes = [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     // 1
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notes.count
     }
     
-    //2
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //1
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
-        //2
-        cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        
+        let row = indexPath.row
+        
+        let note = notes[row]
+        
+        cell.noteTitleLabel.text = note.title
+        
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
         
         return cell
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //1
         if let identifier = segue.identifier {
---
+++ MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,14 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Abby on 8/24/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
